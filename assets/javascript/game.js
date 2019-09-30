287f3275629916e198b32ad532b1f0ea7830e467
Anna Thammakane <anna.thammakane@gmail.com>
2019-09-29 23:10:18 -0500

    updated vars for js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,70 @@
-// establish target number
+$(document).ready(function() {
 
-var targetNumber = 25;
+    // score
+    var targetNumber = 0;
+    var score = 0;
 
-// Here we set the "number-to-guess" header to match the "targetNumber".
-// Eventually this will allow us to change the HTML to match the value in the JavaScript.
-$("#number-to-guess").text(targetNumber);
-
-
-    //counter
-
-    var counter = 0;
+    var win = 0;
+    var losses = 0;
+    var planets = 0;
 
     // Here we created code that selects a number either 10 or 11.
-    // We'll set this number at the start of the "game".
     var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    // var increment = numbers[Math.round(Math.random())];
 
-    // Next we create a for loop to create crystals for every numbers.
-    for (var i = 0; i < numbers.length; i++) {
+    var randomTargetNumber = randomTargetNumber();
 
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "assets/images/widget.jpg");
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
+    function randomeImgValue() {
+        // generate random value for planets
     }
 
+    function randomTargetNumber(){
+        // generate random target value
+        ("#blue-planet")
+        ("#red-planet")
+        ("#yellow-planet")
+        ("#green-planet")
 
+    }
+    function resetGame(){
+
+        score = 0;
+        // Generate random crystal values.
+        planets = randomImgValue();
+        // Generate a random target number and render it to the page.
+        randomNum = randomNumGen();
+        $("#random-area").text(randomNum);
+    }
     // Here we created an on-click event that responds to button clicks of the crystal image.
-    $(".crystal-image").on("click", function() {
+    (".planet-image").on("click", function() {
 
         // Determining the crystal's value requires us to extract the value from the data attribute.
         // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
         // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
         // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
+        var planetValue = ($(this).attr("data-planetvalue"));
+        planetValue = parseInt(planetValue);
         // We then add the crystalValue to the user's "counter" which is a global variable.
         // Every click, from every crystal adds to the global counter.
-        counter += crystalValue;
+        score += planetValue;
 
         // Here we created some logic to "check" if the click counter matches the targetNumber.
         // Remember, this click event will be triggered with each click.
         // With each click the counter will increase by 10 and be re-evaluated against target.
         
-        alert("Total: " + counter);
+        alert("Total: " + score);
 
-        if (counter === targetNumber) {
-
+        if (score === targetNumber) {
             alert("wInNeR! wInNeR! cHiCkEn DiNneR!")
+            wins++;
         }
 
         // Here we added an "else if" condition. If the user's counter ever exceeds the targetNumber...
-        else if (counter >= targetNumber) {
-
+        else if (score > targetNumber) {
         // Then they are alerted with a loss.
             alert("lOsErRrRr!!");
-}
+            losses++;
+        }
 
 // tagetScor
 
@@ -76,4 +72,6 @@ $("#number-to-guess").text(targetNumber);
 // But will then lose immediately if they click again. Seems pointless right?
 // To make this a game we need to create additional code to randomize the counter and targetScore.
 
+    });
 });
+
